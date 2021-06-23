4eceb69753b526506354a14c43b52a8a14f638fa
Ricardo Delgado <ridel007@gmail.com>
2021-06-22 21:45:53 -0500

    First

---
+++ src/main/java/com/rockcor/ProgramacionReactivaDemoWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.rockcor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacionReactivaDemoWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramacionReactivaDemoWebfluxApplication.class, args);
+	}
+
+}
